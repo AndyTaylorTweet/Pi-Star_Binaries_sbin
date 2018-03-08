2ac15f8e14de9fcd7ddc8fc941ec43eef4854799
Andy Taylor <andy@always-online.co.uk>
2018-03-08 16:12:37 +0000

    Update HostFilesUpdate.sh

--- HostFilesUpdate.sh
+++ HostFilesUpdate.sh
@@ -6,7 +6,7 @@
 #      Written for Pi-Star (http://www.pistar.uk/)      #
 #               By Andy Taylor (MW0MWZ)                 #
 #                                                       #
-#                     Version 2.10                      #
+#                     Version 2.20                      #
 #                                                       #
 #   Based on the update script by Tony Corbett G0WFV    #
 #                                                       #
